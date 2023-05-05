9e08fe85753c115be9e49177138b660c3cd37b40
augustan-dev <augustan.dev@gmail.com>
2023-05-05 04:34:03 +0300

    added styles

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -13,24 +13,24 @@ const Navbar = () => {
 	];
 
 	return (
-		<nav className="bg-t_gray-200 sticky w-full h-16 top-0 left-0 flex">
+		<nav className="bg-gray-50 sticky w-full lg:h-16 h-14 top-0 left-0 flex">
 			<img
 				src={logoImg}
 				alt=""
-				className="absolute lg:left-20 left-12 h-auto w-44 navbar"
+				className="absolute lg:left-20 left-12 h-auto lg:w-44 w-40 navbar"
 			/>
-			<div className="float-right flex align-middle lg:mr-24 mr-12 ml-auto my-auto ">
+			<div className="float-right flex content-center justify-center lg:mr-24 mr-12 ml-auto my-auto ">
 				{navbarItems.map((item) => (
 					<a
 						href={item.url}
-						className="rounded-lg px-3 py-2 text-slate-700 font-medium hover:bg-slate-100 hover:text-slate-900 "
+						className="rounded-lg px-3 lg:px-6 py-2 text-slate-700 font-medium hover:bg-slate-100 hover:text-slate-900 "
 					>
 						{item.title}
 					</a>
 				))}
 				<input
 					type="button"
-					className="h-10 w-40 lg:w-96 ml-2 b bg-t_blue-300 "
+					className="h-8 w-auto py-0 px-8 my-auto lg:px-16 lg:text-base lg:h-9 ml-2 lg:ml-6 b bg-t_blue-300 text-cyan-50 rounded-xl start-shadow " value="Start Your Journey" 
 				/>
 			</div>
 		</nav>
